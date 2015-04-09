d9b7113d6e51c4472a446352627922b96873944f
PaoloMessina <ecompliance@Mac-mini-di-eCompliance.local>
2015-04-09 16:59:40 +0200

    Fix error
    AGPushPlugin.h instead of old PushPlugin.h

--- src/ios/AGPushPlugin.m
+++ src/ios/AGPushPlugin.m
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#import "PushPlugin.h"
+#import "AGPushPlugin.h"
 #import "AGDeviceRegistration.h"
 
 @implementation PushPlugin
